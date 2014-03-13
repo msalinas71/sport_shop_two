977d4cb7e98011271fe81dde8c962bb8006350b4
Manuel Salinas <m_salinas@hotmail.es>
2014-03-13 23:09:07 +0100

    Initial commit_semana_1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportShopTwo::Application.config.secret_key_base = '85c0c2e3169cd7a3e084dccd7e6c163c331b554278bd33c36ee8320f300c040f547d347f5716f2282a1b67cf3913455c35dd711dbf7c8493c904dae69f3af096'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SportShopTwo::Application.config.session_store :cookie_store, key: '_sport_shop_two_session'
